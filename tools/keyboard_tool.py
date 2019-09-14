6c6e1a365b53dc3d2a082f499502bed3f30da457
Christopher Kok <ckok@purdue.edu>
2019-09-13 23:17:11 -0500

    Cleaned up keyboard tool

--- tools/keyboard_tool.py
+++ tools/keyboard_tool.py
@@ -1,20 +1,18 @@
 import pyautogui
+import time
 
 def bot_speak(msg):
-    pyautogui.typewrite(msg, interval=0.25)
+    pyautogui.typewrite(msg, interval=0.10)
     pyautogui.press('enter')  # press the Enter key 
 
 def main():
-    # f= open("guru99.txt","w+")
-    # #f=open("guru99.txt","a+")
-    # for i in range(10):
-    #      f.write("This is line %d\r\n" % (i+1))
-    # f.close()
 
-    #Open the file and read the contents
+    # Open the file and read the contents
     f=open("cow_compliments.py", "r")
     fl =f.readlines()
     for x in fl:
-        bot_speak(x)
+        pyautogui.typewrite(x, interval=0.01)
+
 if __name__== "__main__":
+    time.sleep(10)
     main()
