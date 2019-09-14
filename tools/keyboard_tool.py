9b1ee9f172e01903e947ffdd56cd61b75d88b9ce
Christopher Kok <ckok@purdue.edu>
2019-09-14 00:08:59 -0500

    Updated with actual compliments

--- tools/keyboard_tool.py
+++ tools/keyboard_tool.py
@@ -8,10 +8,17 @@ def bot_speak(msg):
 def main():
 
     # Open the file and read the contents
-    f=open("cow_compliments.py", "r")
+    # f=open("cow_compliments.py", "r")
+    # fl =f.readlines()
+    # for x in fl:
+    #     pyautogui.typewrite(x, interval=0.01)    
+
+    f=open("tools/compliments.txt", "r", errors='ignore', encoding='utf-8')
     fl =f.readlines()
+    test = []
     for x in fl:
-        pyautogui.typewrite(x, interval=0.01)
+        test.append(x)
+    print(test)
 
 if __name__== "__main__":
     time.sleep(10)
