45b08bd477b86a314f7184968fe2edbb20e278e0
Christopher Kok <ckok@purdue.edu>
2019-09-14 12:30:48 -0500

    Added compliment writing function

--- tools/keyboard_tool.py
+++ tools/keyboard_tool.py
@@ -13,12 +13,23 @@ def main():
     # for x in fl:
     #     pyautogui.typewrite(x, interval=0.01)    
 
-    f=open("tools/compliments.txt", "r", errors='ignore', encoding='utf-8')
-    fl =f.readlines()
-    test = []
-    for x in fl:
-        test.append(x)
-    print(test)
+    # f=open("tools/compliments.txt", "r", errors='ignore', encoding='utf-8')
+    # fl =f.readlines()
+    # test = []
+    # for x in fl:
+    #     test.append(x)
+    # print(test)
+
+    test_string = ""
+    for i in range(20):
+        for j in range(10):
+            test_string += str(i)
+
+    time.sleep(2)
+    pyautogui.typewrite(test_string, interval=0.01)
+    
+
+
 
 if __name__== "__main__":
     time.sleep(10)
